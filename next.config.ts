ea9a408c35a493c04a6f949acd4bd286cd2b6012
yoonsunny17 <jyoonsun0217@gmail.com>
2025-01-30 00:22:51 +0900

    Update package.json for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/imsunny",
 };
 
 export default nextConfig;
